5f7118f32ed7b24cfa9840d55569909d4833425e
Debanik <44941851+dk1604@users.noreply.github.com>
2024-12-22 22:01:53 +0530

    Update AdaptorFactory.py

--- src/database/AdaptorFactory.py
+++ src/database/AdaptorFactory.py
@@ -1,4 +1,5 @@
 from src.database.MySQLAdapterImpl import MySQLAdapterImpl
+from src.database.PSQLAdapterImpl import PSQLAdapterImpl
 
 
 class AdapterFactory:
@@ -7,9 +8,10 @@ class AdapterFactory:
     def create_adapter(db_type, host, port, user, password, database):
         """Create the appropriate database adapter based on the database type."""
         if db_type.lower() == "mysql":
-            print("\n1..............")
+            print("\ncreate_adaptor called for mysql")
             return MySQLAdapterImpl(host, port, user, password, database)
         elif db_type.lower() == "postgres":
-            return ValueError(f"Postgres database not supported")
+            print("\ncreate_adaptor called for psql")
+            return PSQLAdapterImpl(host, port, user, password, database)
         else:
             raise ValueError(f"Unsupported database type: {db_type}")
