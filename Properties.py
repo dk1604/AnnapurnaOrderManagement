01732b403bf827b8590c72c2adf378c104c94107
Debanik <44941851+dk1604@users.noreply.github.com>
2024-12-22 22:01:34 +0530

    Update Properties.py

--- Properties.py
+++ Properties.py
@@ -1,6 +1,20 @@
-db_type='mysql'
-host='localhost'
-port='3306'
-user='root'
-password='root'
-database='annapurna_caterers'
+import os
+
+# db_type='mysql'
+# host='localhost'
+# port='3306'
+# user='root'
+# password='root'
+# database='annapurna_caterers'
+
+# render
+db_type = os.getenv("db_type")
+host = "dpg-ctk3halumphs73fe6i5g-a"
+port = "5432"
+database = "menu_sjop"
+user = "root"
+password = "TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8"
+internal_database_url = "postgresql://root:TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8@dpg-ctk3halumphs73fe6i5g-a/menu_sjop"
+external_database_url = "postgresql://root:TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8@dpg-ctk3halumphs73fe6i5g-a.oregon-postgres.render.com/menu_sjop"
+psql_command = "PGPASSWORD=TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8 psql -h dpg-ctk3halumphs73fe6i5g-a.oregon-postgres.render.com -U root menu_sjop"
+
