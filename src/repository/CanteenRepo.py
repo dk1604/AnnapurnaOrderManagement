d8b519bc3fd27f994c174166d00a490b71f9b777
Debanik <44941851+dk1604@users.noreply.github.com>
2024-12-24 00:50:23 +0530

    Update CanteenRepo.py

--- src/repository/CanteenRepo.py
+++ src/repository/CanteenRepo.py
@@ -1,7 +1,7 @@
 import logging
 import sys
 
-from src.dao.CanteenDao import fetch_all, fetch_one
+from src.dao.CanteenDao import fetch_all, fetch_one, fetch_all_by_food_category
 from src.database.PSQLAdapterImpl import SessionFactory
 
 
@@ -15,13 +15,29 @@ def handle_exception(exc_type, exc_value, exc_tb):
 sys.excepthook = handle_exception
 
 
-def get_index_route_repo():
+def get_all_options_repo():
     try:
         get_session = SessionFactory.get_session()
         with get_session as session:
             with session.begin():
                 menu_items = fetch_all(session)
                 logging.error("menu_items...........menu_items: %s", menu_items)
+                return menu_items
+    except Exception as ex:
+        session.rollback()
+        logging.error("exception occured in get all %s", str(ex))
+        raise Exception(ex)
+    finally:
+        session.close()
+
+
+def get_all_options_by_food_category_repo(food_category):
+    try:
+        get_session = SessionFactory.get_session()
+        with get_session as session:
+            with session.begin():
+                menu_items = fetch_all_by_food_category(session, food_category)
+                logging.error("food_category menu_items...........menu_items: %s", menu_items)
                 return menu_items
     except Exception as ex:
         session.rollback()
