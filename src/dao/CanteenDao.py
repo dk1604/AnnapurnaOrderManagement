d50a18e262b1c6307b2a6e92945cfc8098cad668
Debanik <44941851+dk1604@users.noreply.github.com>
2024-12-23 14:57:14 +0530

    Update CanteenDao.py

--- src/dao/CanteenDao.py
+++ src/dao/CanteenDao.py
@@ -5,13 +5,13 @@ from sqlalchemy.exc import SQLAlchemyError
 
 def fetch_all(session, model_class):
     if not session:
-        print("No active session. Please connect first.")
+        logging.info("No active session. Please connect first.")
         return None
 
     try:
         results = session.query(model_class).all()
-        print("Fetched all records.")
+        logging.info("Fetched all records.")
         return results
     except SQLAlchemyError as e:
-        print(f"Error fetching all data: {e}")
+        logging.info(f"Error fetching all data: {e}")
         return None
