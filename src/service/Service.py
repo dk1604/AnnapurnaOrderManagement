825fe3a380ade215ceada9f4d53baaeecc355f4d
Debanik <44941851+dk1604@users.noreply.github.com>
2024-12-23 18:52:58 +0530

    Update Service.py

--- src/service/Service.py
+++ src/service/Service.py
@@ -1,6 +1,6 @@
 import logging
 
-from src.repository.CanteenRepo import get_index_route_repo
+from src.repository.CanteenRepo import get_index_route_repo, get_order_item_by_id_repo
 
 logging.basicConfig(level=logging.INFO)
 
@@ -8,3 +8,8 @@ logging.basicConfig(level=logging.INFO)
 def get_index_route_data():
     logging.error("10................")
     return get_index_route_repo()
+
+
+def get_order_item_by_id(item_id):
+    logging.error("10................")
+    return get_order_item_by_id_repo(item_id)
