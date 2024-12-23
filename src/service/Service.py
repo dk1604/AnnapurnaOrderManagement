f89162bf18033577c6ec34ed1430d8bbf9d0b274
Debanik <44941851+dk1604@users.noreply.github.com>
2024-12-23 23:13:55 +0530

    Update Service.py

--- src/service/Service.py
+++ src/service/Service.py
@@ -6,10 +6,8 @@ logging.basicConfig(level=logging.INFO)
 
 
 def get_index_route_data():
-    logging.error("10................")
     return get_index_route_repo()
 
 
 def get_order_item_by_id(item_id):
-    logging.error("10................")
     return get_order_item_by_id_repo(item_id)
