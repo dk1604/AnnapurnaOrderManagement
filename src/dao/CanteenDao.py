f6716a7def66c9f5a07912adfb139a872773ba24
Debanik <44941851+dk1604@users.noreply.github.com>
2024-12-23 19:05:13 +0530

    Update CanteenDao.py

--- src/dao/CanteenDao.py
+++ src/dao/CanteenDao.py
@@ -10,17 +10,17 @@ def fetch_all(session, model_class):
         records = session.query(model_class).all()
         logging.error("Fetched all records.")
 
-        # response_list = [
-        #     menu_table_response(id=row[0].id, name=row[0].name, description=row[0].description, price=row[0].price
-        #                         ).model_dump() for row in records
-        # ]
         response_list = [
-            menu_table_response(id=1, name='Chicken Biryani', description='Indian - Biryani', price=90),
-            menu_table_response(id=2, name='Veg Biryani', description='Indian - Biryani', price=80),
-            menu_table_response(id=3, name='Chicken Thali', description='Indian - Thali', price=70),
-            menu_table_response(id=3, name='Veg Thali', description='Indian - Thali', price=60)
+            menu_table_response(id=row[0].id, name=row[0].name, description=row[0].description, price=row[0].price
+                                ).model_dump() for row in records
         ]
-        logging.error("Fetched response_list %s", response_list)
+        logging.error("Fetched response_list_db %s", response_list)
+        # response_list = [
+        #     menu_table_response(id=1, name='Chicken Biryani', description='Indian - Biryani', price=90),
+        #     menu_table_response(id=2, name='Veg Biryani', description='Indian - Biryani', price=80),
+        #     menu_table_response(id=3, name='Chicken Thali', description='Indian - Thali', price=70),
+        #     menu_table_response(id=3, name='Veg Thali', description='Indian - Thali', price=60)
+        # ]
         return response_list
     except Exception as e:
         logging.error(f"Error fetching all data: {e}")
