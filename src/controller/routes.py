a4ac47706fcac6f2d59e408d4448ccf5bc95e43a
Debanik <44941851+dk1604@users.noreply.github.com>
2024-12-23 23:18:21 +0530

    Update routes.py

--- src/controller/routes.py
+++ src/controller/routes.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import sys
 
 from flask import request, redirect, url_for, render_template, Flask
 from flask.sansio.blueprints import Blueprint
@@ -9,6 +10,16 @@ from src.service.Service import get_index_route_data, get_order_item_by_id
 routes_blueprint = Blueprint("routes", __name__)
 logging.basicConfig(level=logging.INFO)
 
+
+def handle_exception(exc_type, exc_value, exc_tb):
+    if exc_type == KeyboardInterrupt:
+        sys.__excepthook__(exc_type, exc_value, exc_tb)
+    else:
+        # Custom behavior for uncaught exceptions (e.g., log it without printing the full stack trace)
+        print(f"Handled exception: {exc_value}")
+
+
+sys.excepthook = handle_exception
 
 template_folder = os.path.join(os.getcwd(), 'src', 'templates')
 static_folder = os.path.join(os.getcwd(), 'src', 'static')
