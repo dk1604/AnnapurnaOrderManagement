7db25d916c6ecf4921da66bfe3e95fb865bd4236
Debanik <44941851+dk1604@users.noreply.github.com>
2024-12-23 23:16:07 +0530

    Update routes.py

--- src/controller/routes.py
+++ src/controller/routes.py
@@ -22,34 +22,28 @@ def configure_routes(app):
         try:
             menu_items = get_index_route_data()
 
-            # conn.disconnect()
             if not menu_items:  # If the list is empty
                 logging.error("menu items are none")
-                # return "hello 1"
                 return render_template('index.html', menu_items=None)  # Pass `None` or empty list
 
             # If the menu is not empty, render the template with data
             logging.error("menu_items........", menu_items)
             return render_template('index.html', menu_items=menu_items)
-            # return "hello 2"
         except Exception as e:
             logging.error("exception at getting index......%s", str(e))
 
     # Order route
     @app.route('/order/<int:item_id>', methods=['GET', 'POST'])
     def order(item_id):
-        logging.error("100..........", item_id)
         item = get_order_item_by_id(item_id)
         logging.error("item............%s", item)
 
         if request.method == 'POST':
-            logging.error("101................")
             # Here you can add order functionality (e.g., save the order)
             name = request.form['name']
             quantity = request.form['quantity']
-            logging.error("name....", name)
-            logging.error("quantity....", quantity)
-            logging.error("item....", item.name)
+            logging.error("name, quantity, item....%s, %s, %s", name, quantity, item.name)
+
             # You can save this to an "orders" table or send a confirmation email
             return redirect(url_for('index'))
 
