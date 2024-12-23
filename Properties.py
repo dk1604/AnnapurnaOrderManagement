e8a165488678c3179777bc57ef1f20b10c1d8190
Debanik <44941851+dk1604@users.noreply.github.com>
2024-12-23 14:38:08 +0530

    Update Properties.py

--- Properties.py
+++ Properties.py
@@ -19,7 +19,7 @@ import os
 db_type = os.getenv("db_type").lower()
 host = "dpg-ctk3halumphs73fe6i5g-a"
 port = "5432"
-database = "menu_sjop"
+database = "menu"
 user = "root"
 password = "TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8"
 internal_database_url = "postgresql://root:TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8@dpg-ctk3halumphs73fe6i5g-a/menu_sjop"
