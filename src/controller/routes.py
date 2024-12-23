3fa81468da8fbd4c7ec530c9d642be41d57eefea
Debanik <44941851+dk1604@users.noreply.github.com>
2024-12-23 14:19:28 +0530

    Create routes.py

---
+++ src/controller/routes.py
@@ -0,0 +1,56 @@
+import logging
+import os
+
+from flask import request, redirect, url_for, render_template, Flask
+from flask.sansio.blueprints import Blueprint
+
+from src.service.Service import get_index_route_data
+
+routes_blueprint = Blueprint("routes", __name__)
+logging.basicConfig(level=logging.INFO)
+
+
+template_folder = os.path.join(os.getcwd(), 'src', 'templates')
+static_folder = os.path.join(os.getcwd(), 'src', 'static')
+app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+
+
+def configure_routes(app):
+    @app.route('/')
+    def index():
+        print("\ninside default route")
+        try:
+            menu_items = get_index_route_data()
+
+            # conn.disconnect()
+            if not menu_items:  # If the list is empty
+                print("\nmenu items are none")
+                # return "hello"
+                return render_template('index.html', menu_items=None)  # Pass `None` or empty list
+
+            # If the menu is not empty, render the template with data
+            return render_template('index.html', menu_items=menu_items)
+            # return "hello"
+        except Exception as e:
+            logging.info("exception at getting index......%s", str(e))
+
+    # Order route
+    # @app.route('/order/<int:item_id>', methods=['GET', 'POST'])
+    # def order(item_id):
+    #     logging.info("\n100..........", item_id)
+    #     conn = get_db_connection()
+    #     item = conn.fetch_one('SELECT * FROM menu WHERE id = %s', (item_id,))
+    #     logging.info("item............%s", item)
+    #
+    #     if request.method == 'POST':
+    #         logging.info("\n101................")
+    #         # Here you can add order functionality (e.g., save the order)
+    #         name = request.form['name']
+    #         quantity = request.form['quantity']
+    #         print("\nname....", name)
+    #         print("\nquantity....", quantity)
+    #         print("\nitem....", item['name'])
+    #         # You can save this to an "orders" table or send a confirmation email
+    #         return redirect(url_for('index'))
+    #
+    #     return render_template('order.html', item=item)
