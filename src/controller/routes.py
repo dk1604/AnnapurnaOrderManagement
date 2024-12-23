5793e196f240ae57a57dcf72f7f414f9853e7f06
Debanik <44941851+dk1604@users.noreply.github.com>
2024-12-23 17:11:21 +0530

    Update routes.py

--- src/controller/routes.py
+++ src/controller/routes.py
@@ -7,22 +7,24 @@ from flask.sansio.blueprints import Blueprint
 from src.service.Service import get_index_route_data
 
 routes_blueprint = Blueprint("routes", __name__)
+logging.basicConfig(level=logging.INFO)
+
 
 template_folder = os.path.join(os.getcwd(), 'src', 'templates')
 static_folder = os.path.join(os.getcwd(), 'src', 'static')
 app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
-app.logger.setLevel(logging.INFO)
+
 
 def configure_routes(app):
     @app.route('/')
     def index():
-        app.logger.error("inside default route")
+        logging.error("inside default route")
         try:
             menu_items = get_index_route_data()
 
             # conn.disconnect()
             if not menu_items:  # If the list is empty
-                app.logger.error("menu items are none")
+                logging.error("menu items are none")
                 # return "hello 1"
                 return render_template('index.html', menu_items=None)  # Pass `None` or empty list
 
@@ -30,7 +32,7 @@ def configure_routes(app):
             return render_template('index.html', menu_items=menu_items)
             # return "hello 2"
         except Exception as e:
-            app.logger.error("exception at getting index......%s", str(e))
+            logging.error("exception at getting index......%s", str(e))
 
     # Order route
     # @app.route('/order/<int:item_id>', methods=['GET', 'POST'])
