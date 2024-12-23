87fc0b8a752add1c2d776462a18b98d71c26575c
Debanik <44941851+dk1604@users.noreply.github.com>
2024-12-23 18:17:55 +0530

    Update CanteenDao.py

--- src/dao/CanteenDao.py
+++ src/dao/CanteenDao.py
@@ -8,6 +8,7 @@ from src.models.CanteenModels import menu_table_response
 
 def fetch_all(session, model_class):
     try:
+        logging.error("model_class is: ", model_class)
         records = session.query(model_class).all()
         logging.error("Fetched all records.")
 
@@ -15,8 +16,7 @@ def fetch_all(session, model_class):
             menu_table_response(id=row[0].id, name=row[0].name, description=row[0].description, price=row[0].price
                                 ).model_dump() for row in records
         ]
-        logging.error("Fetched response_list first")
-        logging.error("Fetched response_list second", response_list)
+        logging.error("Fetched response_list %s", response_list)
         return response_list
     except Exception as e:
         logging.error(f"Error fetching all data: {e}")
