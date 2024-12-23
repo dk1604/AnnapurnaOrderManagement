79b76d299e722a6546eae501d94d5b7acdc743ba
Debanik <44941851+dk1604@users.noreply.github.com>
2024-12-23 14:45:16 +0530

    Update Properties.py

--- Properties.py
+++ Properties.py
@@ -19,10 +19,10 @@ import os
 db_type = os.getenv("db_type").lower()
 host = "dpg-ctk3halumphs73fe6i5g-a"
 port = "5432"
-database = "menu"
+database = "menu_sjop"
 user = "root"
 password = "TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8"
-internal_database_url = "postgresql://root:TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8@dpg-ctk3halumphs73fe6i5g-a/menu_sjop"
-external_database_url = "postgresql://root:TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8@dpg-ctk3halumphs73fe6i5g-a.oregon-postgres.render.com/menu_sjop"
-psql_command = "PGPASSWORD=TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8 psql -h dpg-ctk3halumphs73fe6i5g-a.oregon-postgres.render.com -U root menu_sjop"
+internal_database_url = "postgresql://root:TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8@dpg-ctk3halumphs73fe6i5g-a/menu"
+external_database_url = "postgresql://root:TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8@dpg-ctk3halumphs73fe6i5g-a.oregon-postgres.render.com/menu"
+psql_command = "PGPASSWORD=TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8 psql -h dpg-ctk3halumphs73fe6i5g-a.oregon-postgres.render.com -U root menu"
 
