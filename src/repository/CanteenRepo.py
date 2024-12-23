1595ba6a28a7e203f02412b7e1d54fd2e65a115c
Debanik <44941851+dk1604@users.noreply.github.com>
2024-12-23 18:52:29 +0530

    Update CanteenRepo.py

--- src/repository/CanteenRepo.py
+++ src/repository/CanteenRepo.py
@@ -1,6 +1,6 @@
 import logging
 
-from src.dao.CanteenDao import fetch_all
+from src.dao.CanteenDao import fetch_all, fetch_one
 from src.database.PSQLAdapterImpl import SessionFactory, Menu
 
 
@@ -21,6 +21,25 @@ def get_index_route_repo():
     except Exception as ex:
         session.rollback()
         logging.error("exception occured in get all %s", str(ex))
+        raise Exception(ex)
+    finally:
+        session.close()
+
+
+def get_order_item_by_id_repo(item_id):
+    try:
+        logging.error("41.............")
+        get_session = SessionFactory.get_session()
+        with get_session as session:
+            with session.begin():
+                logging.error("42.............")
+                menu_items = fetch_one(session, item_id)
+                logging.error("43.........")
+                logging.error("44...........menu_items: %s", menu_items)
+                return menu_items
+    except Exception as ex:
+        session.rollback()
+        logging.error("45..exception occured in get all %s", str(ex))
         raise Exception(ex)
     finally:
         session.close()
