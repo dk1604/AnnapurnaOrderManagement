397bb04f7cbda53105caa0f0c6f16a4d04f02047
Debanik <44941851+dk1604@users.noreply.github.com>
2024-12-31 11:13:19 +0530

    Update Properties.py

--- Properties.py
+++ Properties.py
@@ -29,3 +29,5 @@ internal_database_url = "postgresql://root:TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8@dpg-
 external_database_url = "postgresql://root:TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8@dpg-ctk3halumphs73fe6i5g-a.oregon-postgres.render.com/menu"
 psql_command = "PGPASSWORD=TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8 psql -h dpg-ctk3halumphs73fe6i5g-a.oregon-postgres.render.com -U root menu"
 
+time_to_nullify_session = os.getenv("time_to_nullify_session")
+
