a768b106268a273918e6921d084f61e111b58583
Debanik <44941851+dk1604@users.noreply.github.com>
2025-01-01 16:31:24 +0530

    Update Properties.py

--- Properties.py
+++ Properties.py
@@ -30,4 +30,5 @@ external_database_url = "postgresql://root:TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8@dpg-
 psql_command = "PGPASSWORD=TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8 psql -h dpg-ctk3halumphs73fe6i5g-a.oregon-postgres.render.com -U root menu"
 
 time_to_nullify_session = os.getenv("time_to_nullify_session")
+base_url = "https://annapurnaordermanagement.onrender.com"
 
