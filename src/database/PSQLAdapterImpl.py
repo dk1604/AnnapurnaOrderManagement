61833e6a7367cd116286e4111b230704c48b55b9
Debanik <44941851+dk1604@users.noreply.github.com>
2025-01-05 16:46:14 +0530

    Update PSQLAdapterImpl.py

--- src/database/PSQLAdapterImpl.py
+++ src/database/PSQLAdapterImpl.py
@@ -2,7 +2,7 @@ import logging
 import sys
 
 from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import Column, String, Integer, VARCHAR, create_engine
+from sqlalchemy import Column, String, Integer, VARCHAR, create_engine, Numeric, Date
 from sqlalchemy.orm import declarative_base
 from Properties import user, password, host, port, database
 
@@ -24,14 +24,27 @@ class CustomAlembicVersion(Base):
     version_num = Column(String(32), primary_key=True)
 
 
-class Menu(Base):
+class menu(Base):
     __tablename__ = "menu"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(VARCHAR(250))
+    name = Column(VARCHAR(250), unique=True, nullable=False)
     description = Column(VARCHAR(250))
     price = Column(Integer)
     food_category = Column(VARCHAR(100))
+
+
+class CustomerOrder(Base):
+    __tablename__ = "customer_order"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    order_id = Column(String(100), unique=True, nullable=False)
+    order_details = Column(String(500), nullable=False)
+    cart_price = Column(Numeric(precision=10, scale=2), nullable=False)
+    user_name = Column(String(250), nullable=True)
+    user_phone = Column(String(100), nullable=True)
+    order_date = Column(Date, nullable=False)
+
 
 
 class PSQLAdapterImpl:
