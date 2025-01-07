c96d2b62f5fb749f6f967b3f09899133c5380164
Debanik <44941851+dk1604@users.noreply.github.com>
2025-01-07 15:39:47 +0530

    Update CanteenDao.py

--- src/dao/CanteenDao.py
+++ src/dao/CanteenDao.py
@@ -1,8 +1,9 @@
 import logging
 import sys
 
-from src.database.PSQLAdapterImpl import Menu
-from src.models.CanteenModels import menu_table_response
+from src.database.PSQLAdapterImpl import Menu, CustomerOrder
+from src.models.CanteenModels import menu_table_response, customer_order_details, customer_order_details_response
+
 
 def handle_exception(exc_type, exc_value, exc_tb):
     if exc_type == KeyboardInterrupt:
@@ -61,3 +62,38 @@ def fetch_one(session, item_id):
         logging.error(f'error occurred during get by id dao: {str(err)}')
         raise ValueError(f"Error: {err}")
 
+
+def save_order_details(session, event):
+    try:
+        session.add(event)
+        session.flush()
+        response = customer_order_details(id=event.id,
+                                          order_id=event.order_id,
+                                          order_details=event.order_details,
+                                          cart_price=event.cart_price,
+                                          payment_mode=event.payment_mode,
+                                          user_name=event.user_name,
+                                          user_phone=event.user_phone,
+                                          order_date=event.order_date)
+        logging.error(f'customer order detail record saved successfully: {response}')
+        return response
+
+    except Exception as dbError:
+        session.rollback()
+        logging.error(f'Error during save: {str(dbError)}')
+        raise None
+
+
+def get_order_details_dao_by_time(session, start_time, end_time):
+    try:
+        records = session.query(CustomerOrder).filter(CustomerOrder.order_date >= start_time).filter(CustomerOrder.order_date <= end_time).all()
+        logging.error("Fetched all records.")
+
+        response_list = [
+            customer_order_details_response(id=row.id, order_details=row.order_details, cart_price=row.cart_price, payment_mode=row.payment_mode, user_phone=row.user_phone, order_date=row.order_date) for row in records
+        ]
+        logging.error("Fetched response_list_db %s", response_list)
+        return response_list
+    except Exception as e:
+        logging.error(f"Error fetching all data: {e}")
+        return None
