ed3a2db019359d2592363aad25d87243f0636ba6
Debanik <44941851+dk1604@users.noreply.github.com>
2025-01-07 15:40:51 +0530

    Update CanteenModels.py

--- src/models/CanteenModels.py
+++ src/models/CanteenModels.py
@@ -1,3 +1,4 @@
+from sqlite3 import Date
 from typing import Optional
 
 from pydantic import BaseModel, ConfigDict
@@ -9,3 +10,23 @@ class menu_table_response(BaseModel):
     #description: str
     price: int
     id: Optional[int] = None
+
+
+class customer_order_details(BaseModel):
+    order_id: str
+    order_details: str
+    cart_price: int
+    payment_mode: str
+    user_name: str
+    user_phone: str
+    order_date: int
+    id: Optional[int] = None
+
+
+class customer_order_details_response(BaseModel):
+    order_details: str
+    cart_price: int
+    payment_mode: str
+    user_phone: str
+    order_date: int
+
