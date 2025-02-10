43342c55c757dfa7af83ad4eef57f118bc3b197f
Debanik <44941851+dk1604@users.noreply.github.com>
2025-02-10 22:54:02 +0530

    created new db Properties.py

--- Properties.py
+++ Properties.py
@@ -20,14 +20,14 @@ client_id = os.getenv("client_id")
 client_secret = os.getenv("client_secret")
 
 db_type = os.getenv("db_type").lower()
-host = "dpg-ctk3halumphs73fe6i5g-a"
+host = "dpg-cul3akjqf0us738pqrh0-a.oregon-postgres.render.com"
 port = "5432"
-database = "menu_sjop"
+database = "menu_i8cw"
 user = "root"
-password = "TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8"
-internal_database_url = "postgresql://root:TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8@dpg-ctk3halumphs73fe6i5g-a/menu"
-external_database_url = "postgresql://root:TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8@dpg-ctk3halumphs73fe6i5g-a.oregon-postgres.render.com/menu"
-psql_command = "PGPASSWORD=TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8 psql -h dpg-ctk3halumphs73fe6i5g-a.oregon-postgres.render.com -U root menu"
+password = "HwVzJAZ12Okh827U3ErYVFUc5y4w3s2r"
+internal_database_url = "postgresql://root:HwVzJAZ12Okh827U3ErYVFUc5y4w3s2r@dpg-cul3akjqf0us738pqrh0-a/menu_i8cw"
+external_database_url = "postgresql://root:TYKxWnrB2utOK65pHKq0Xuh1YeDngwm8@dpg-ctk3halumphs73fe6i5g-a.oregon-postgres.render.com/menu_sjop"
+psql_command = "PGPASSWORD=HwVzJAZ12Okh827U3ErYVFUc5y4w3s2r psql -h dpg-cul3akjqf0us738pqrh0-a.oregon-postgres.render.com -U root menu_i8cw"
 
 time_to_nullify_session = os.getenv("time_to_nullify_session")
 base_url = "https://annapurnaordermanagement.onrender.com"
