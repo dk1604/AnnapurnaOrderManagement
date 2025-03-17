be71f8f90289401c1acd1ee31221f65d7a35d8fc
Debanik <44941851+dk1604@users.noreply.github.com>
2025-03-18 00:00:41 +0530

    Update Service.py

--- src/service/Service.py
+++ src/service/Service.py
@@ -13,7 +13,7 @@ def get_all_options_by_food_category(food_category):
     match food_category:
         case "all":
             return CanteenRepo.get_all_options_repo()
-        case "veg" | "non_veg" | "desert" | "chinese" | "egg":
+        case "veg" | "non_veg" | "desert" | "chinese" | "egg" | "snacks":
             return CanteenRepo.get_all_options_by_food_category_repo(food_category)
         case _:
             return "Invalid category. Please choose from 'all', 'veg', 'non_veg', 'chinese' or 'desert'."
