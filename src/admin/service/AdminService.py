50324a290e8d0a92df1b1b9c1263fe4a064de393
Debanik <44941851+dk1604@users.noreply.github.com>
2026-03-16 01:15:43 +0530

    Update AdminService.py

--- src/admin/service/AdminService.py
+++ src/admin/service/AdminService.py
@@ -20,11 +20,32 @@ def get_vendor_expense_service(page, page_size):
     paginated_data = vendor_expense[offset: offset + page_size]
     total_pages = math.ceil(total_records / page_size)
 
+    total_expense = 0
+    total_paid = 0
+    total_balance  = 0
+
+    for expense in vendor_expense:
+        payment_mode = expense.get("payment_mode")
+        amount = float(expense.get("amount", 0))
+
+        if payment_mode in ["purchase_pay_cash", "purchase_pay_online"]:
+            total_expense += amount
+            total_paid += amount
+        elif payment_mode == "purchase_on_credit":
+            total_expense += amount
+        elif payment_mode in ["vendor_payment_cash", "vendor_payment_online"]:
+            total_paid += amount
+
+    total_balance = total_expense - total_paid
+
     return {
         "page": page,
         "page_size": page_size,
         "total_pages": total_pages,
-        "data": paginated_data
+        "data": paginated_data,
+        "total_expense": total_expense,
+        "total_paid": total_paid,
+        "total_balance": total_balance
     }
 
 def get_all_menu_service():
